d581bbea6623db673b4ac70c9e5fd97809a6f0f7
waynekhien <khien23092003@gmail.com>
2025-09-13 12:02:04 +0700

    signup controller added

---
+++ backend/src/models/user.model.js
@@ -0,0 +1,29 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    email: {
+      type: String,
+      require: true,
+      unique: true,
+    },
+    fullName: {
+      type: String,
+      require: true,
+    },
+    password: {
+      type: String,
+      require: true,
+      minlength: 6,
+    },
+    profilePic: {
+      type: String,
+      default: "",
+    },
+  },
+  { timestamps: true } // createdAt & updatedAt
+);
+
+const User = mongoose.model("User", userSchema)
+
+export default User
--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,14 +1,13 @@
 import express from "express";
+import { signup } from "../controllers/auth.controller.js";
 
 const router = express.Router();
 
-router.get("/signup", (req, res) => {
-    res.send("Signup endpoint");
-});
-router.get("/login", (req, res) => {
+router.post("/signup", signup);
+router.post("/login", (req, res) => {
     res.send("Login endpoint");
 });
-router.get("/logout", (req, res) => {
+router.post("/logout", (req, res) => {
     res.send("Logout endpoint");
 });
 
