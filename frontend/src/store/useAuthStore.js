3d30b693f002b4c99049153c7224334a81a2314b
waynekhien <khien23092003@gmail.com>
2025-09-16 16:02:49 +0700

    Update backend URLs for production

--- frontend/src/store/useAuthStore.js
+++ frontend/src/store/useAuthStore.js
@@ -4,7 +4,7 @@ import toast from "react-hot-toast";
 import { io } from "socket.io-client";
 
 const BASE_URL =
-  import.meta.env.MODE === "development" ? "http://localhost:3000" : "/";
+  import.meta.env.MODE === "development" ? "http://localhost:3000" : "https://chat-app-backend-lqld.onrender.com";
 
 export const useAuthStore = create((set, get) => ({
   authUser: null,
