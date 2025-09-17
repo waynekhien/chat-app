d2b14a8d1f5eca8ecd22b9061aba7261facd6b70
waynekhien <khien23092003@gmail.com>
2025-09-17 16:01:14 +0700

    add api backend

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-    baseURL: "/api",
+    baseURL: import.meta.env.MODE === "development" ? "/api" : "https://chat-app-backend-lqld.onrender.com/api",
     withCredentials: true
 })
